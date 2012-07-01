a541d340cee1e6db1126daef2dfc0ba3f3135e4f
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2012-07-01 19:55:01 +0200

    Not everyone uses github

--- lib/capistrano/ext/git.rb
+++ lib/capistrano/ext/git.rb
@@ -6,5 +6,5 @@ end
 
 Capistrano::Configuration.instance(:must_exist).load do
   set :repository,
-    fetch(:project_repository, `git remote show -n origin | awk '/git@github/{print $3}' | head -n 1`.strip)
+    fetch(:project_repository, `git remote show -n origin | grep 'URL' | awk '{print $3}' | head -n 1`.strip)
 end
